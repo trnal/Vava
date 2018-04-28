43a6f50e19ed23eccd4ac5de7c9f057b6fe661f2
mike1243 <michal.fabis01@gmail.com>
2018-04-28 20:14:57 +0200

    updated gitignore

--- src/main/java/handyman/OrderController.java
+++ src/main/java/handyman/OrderController.java
@@ -1,15 +1,11 @@
 package handyman;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import handyman.models.Order;
 
 @Controller
 public class OrderController {
