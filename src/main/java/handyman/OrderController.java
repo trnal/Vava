b6e19d1d8ab60af0b390188b4e5d1d2aadfa7262
mike1243 <michal.fabis01@gmail.com>
2018-04-29 23:21:44 +0200

    Logging configured

--- src/main/java/handyman/OrderController.java
+++ src/main/java/handyman/OrderController.java
@@ -5,20 +5,19 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
-import com.google.gson.Gson;
-
-import handyman.models.Order;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import handyman.repositories.OrderRepository;
 import handyman.services.OrderService;
-
+import com.google.gson.Gson;
+import handyman.models.Order;
 
 @Controller
 public class OrderController {
+	private static Logger LOG = Logger.getLogger(OrderController.class.getName());
+	
 	@Autowired
 	private OrderRepository orderRepository;
 	@Autowired
@@ -26,6 +25,7 @@ public class OrderController {
 	
 	@RequestMapping(path="/order/index", method = RequestMethod.GET)
     public String index(Model model) {
+		LOG.log(Level.INFO, "Zobrazenie všetkých požiadaviek od používateľov");
 		List<Order> orders = orderService.getAllOrders();
 		
 		Gson gsonparser = new Gson();
@@ -36,19 +36,34 @@ public class OrderController {
 	
 	@RequestMapping(path="/order", method = RequestMethod.GET)
     public String order(Model model) {
+		LOG.log(Level.INFO, "Otvorenie formulára s vytvorením požiadavky");
+		
         return "order/createorder";
     }
 	
 	@RequestMapping(path="/order/store", method = RequestMethod.POST)
     public String store(Model model, @RequestParam("name") String name, @RequestParam("description") String description, @RequestParam("phoneNumber") String phoneNumber, @RequestParam("address") String address, @RequestParam("town") String town, @RequestParam("coordLon") Double coordLon, @RequestParam("coordLat") Double coordLat) {
 		Order newOrder = new Order(name, description, address, phoneNumber, town, coordLon, coordLat);	
-		orderRepository.save(newOrder);
+		LOG.log(Level.INFO, "Začiatok ukladania požiadavky do databázy");
+		try {
+			orderRepository.save(newOrder);
+		} catch (Exception e) {
+			LOG.log(Level.SEVERE, "Požiadavka nemohla byť správne uložená do databázy", e);
+		}
+		LOG.log(Level.INFO, "Presmerovanie na obrazovku so všetkými požiadavkami");
 		return "redirect:/order/index";
     }
 	
 	@RequestMapping(path="/order-detail/{id}", method = RequestMethod.GET)
 	public String show(Model model, @PathVariable(value = "id")Long id) {
-		Order order = orderService.getOrderById(id);
+		LOG.log(Level.INFO, "Otvorenie detailu s id " + id);
+		Order order  = null;
+		try {
+			order = orderService.getOrderById(id);	
+		} catch(Exception e) {
+			LOG.log(Level.SEVERE, "Používateľa s id " + id + " sa nepodarilo vybrať z databázy", e);
+		}
+		
 		model.addAttribute("order", order);
 		return "order/showOrder";
 	}
